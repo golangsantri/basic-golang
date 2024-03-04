e6855389f55715761d31a155d41dda96d9a80791
Golang Santri <muriarfad@gmail.com>
2024-03-04 14:28:12 +0700

    Update: learning interface part 1 has complete

--- 13. Interface.go
+++ 13. Interface.go
@@ -1,10 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
 type Test interface {
-	float() float32
-	number() uint
+	Luas() float64
+	keliling() float64
+}
+
+type Lingkaran struct {
+	Diameter float32
+}
+func (l Lingkaran) JariJari()float32  {
+	return l.Diameter / 2
+}
+func (l Lingkaran) Luas()float64  {
+	return math.Pi * math.Pow(float64(l.JariJari()),float64(2))
+}
+func (l Lingkaran) keliling()float64  {
+	return math.Pi * float64(math.Pi)
+}
+type Persegi_panjang struct {
+	Panjang uint
+	Lebar uint
+}
+func (p Persegi_panjang) Luas()float64  {
+	return float64(p.Panjang) * float64(p.Lebar)
+}
+func (p Persegi_panjang) keliling()float64  {
+	return float64(2 * (p.Lebar + p.Panjang))
 }
 func main() {
-	
+	var bangunDatar Test
+	bangunDatar = Lingkaran{
+		Diameter: 10,
+	}
+	fmt.Println("Luas: ",bangunDatar.Luas())
+	fmt.Println("Keliling: ", bangunDatar.keliling())
+	fmt.Println("Jari-jari: ",bangunDatar.(Lingkaran).JariJari())
+	fmt.Println("Persegi")
+	bangunDatar = Persegi_panjang{
+		Panjang: 10,
+		Lebar: 5,
+	}
+	fmt.Println("Luas: ",bangunDatar.Luas())
+	fmt.Println("Keliling", bangunDatar.keliling())
+	fmt.Println("Panjang persegi: ",bangunDatar.(Persegi_panjang).Panjang)
 }
-t
